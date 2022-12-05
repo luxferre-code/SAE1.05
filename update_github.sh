93cb49ba3f549a06989f7c0a83f2f699f10c97e8
Valentin Thuillier <valentinrthuillier@gmail.com>
2022-12-05 14:29:26 +0100

    Adding redirection to official ankama website

--- update_github.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git pull origin main
-git init
-git add .
-git commit -m "Update"
-git branch -M main
-git remote add origin https://github.com/luxferre-code/SAE1.05.git
-git push -u origin main
